1592dd3abf57609676809fd779068bf800e2026a
itexpert120 <itexpert120@gmail.com>
2023-06-20 12:35:02 +0500

    PWA fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,30 +7,32 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: "prompt",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
       manifest: {
         name: "Calculator",
         short_name: "Calculator",
         description: "iOS Calculator Clone",
+        theme_color: "#ffffff",
+        start_url: "/",
         icons: [
           {
-            src: "/android-chrome-192x192.png",
+            src: "android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/android-chrome-512x512.png",
+            src: "android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
         ],
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-        scope: "/",
-        start_url: "/",
-        orientation: "portrait",
       },
     }),
   ],
